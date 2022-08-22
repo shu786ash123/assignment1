a7c0391e5c5b02044d0c18f438a2d533444a5b6c
ASHISH SHUKLA <106545357+shu786ash123@users.noreply.github.com>
2022-08-23 00:46:11 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+       int t,h;
+       printf("enter time \n ");
+       scanf("%d %d",&t,&h);
+       printf("%d HOUR and %d MINUTE",t,h);
+       return 0;
+}
+
