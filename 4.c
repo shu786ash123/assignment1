3164212dbcba9bac261f208879483c9c3dbe5e4b
ASHISH SHUKLA <106545357+shu786ash123@users.noreply.github.com>
2022-08-23 00:25:23 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int r,a;
+    
+    printf("ENTER RADIUS OF CIRCLE");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("AREA OF CIRCLE HAVING RADIUS %d IS %d",r,a);
+    return 0;
+}
