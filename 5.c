724a0b4d5a177826d8860bf564201cad2f94a317
ASHISH SHUKLA <106545357+shu786ash123@users.noreply.github.com>
2022-08-23 00:26:04 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,10 @@
+  //calculate length of string using printf()
+  #include<stdio.h>
+  int main()
+  {
+    int x;
+   x = printf("ASHISH");
+    printf("lenghth of string is %d",x);
+    return 0;
+  }
+  
